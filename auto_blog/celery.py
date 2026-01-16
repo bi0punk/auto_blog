663fb475122576e7ffaaca6dfc00c504aca300e6
bi0punk <systecinfo_cl@criptext.com>
2026-01-15 23:30:27 -0300

    re-order

--- auto_blog/celery.py
+++ auto_blog/celery.py
@@ -20,21 +20,3 @@ def debug_task(self):
     print(f'Request: {self.request!r}')
 
 
-""" from __future__ import absolute_import, unicode_literals
-import os
-from celery import Celery
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'auto_blog.settings')
-
-app = Celery('auto_blog')
-
-app.config_from_object('django.conf:settings', namespace='CELERY')
-
-# Asegúrate de que esta línea esté presente en tu configuración
-app.conf.broker_connection_retry_on_startup = True
-
-app.autodiscover_tasks()
-
-@app.task(bind=True)
-def debug_task(self):
-    print(f'Request: {self.request!r}') """
--- blog/__init__.py
+++
@@ -1,7 +0,0 @@
-# __init__.py
-
-from __future__ import absolute_import, unicode_literals
-#aplicación de Celery se cargue cuando Django inicie
-from .celery import app as celery_app
-
-__all__ = ('celery_app',)
